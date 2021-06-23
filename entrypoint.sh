81de0322c7f5f1c3eed4a845bcb46805457c8836
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-23 14:51:54 +0200

    msg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,28 @@
 #!/bin/bash -l
 
 echo "Hello sir"
+
+echo "install nvm"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+echo "install nvm"
+nvm install node 14
 node --version
 
-echo "change npm's default directory"
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-export PATH=~/.npm-global/bin:$PATH
-source ~/.bash_profile
+# echo "change npm's default directory"
 
-echo "installing inso"
-npm install -g insomnia-inso
 
-echo "inso version"
-inso --version
+# echo "installing inso"
+# npm install -g insomnia-inso
 
-echo "linting"
-run: inso lint spec "swagger.json" --ci
+# echo "inso version"
+# inso --version
 
-echo "Generate declarative config"
-inso generate config "swagger.json" --type declarative | tee kong.yml
+# echo "linting"
+# run: inso lint spec "swagger.json" --ci
 
-echo "file:"
-cat kong.yml
+# echo "Generate declarative config"
+# inso generate config "swagger.json" --type declarative | tee kong.yml
+
+# echo "file:"
+# cat kong.yml
