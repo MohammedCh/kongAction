df185872acd51c6b1f6a99ce628e7b91aa7709b6
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-23 12:38:04 +0200

    msg

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh -l
 
 echo "Hello sir"
+node --version
+
+echo "installing inso"
+npm install -g insomnia-inso
+
+echo "linting"
+run: inso lint spec "swagger.json" --ci
+
+echo "Generate declarative config"
+inso generate config "swagger.json" --type declarative | tee kong.yml
+
+echo "file:"
+cat kong.yml
