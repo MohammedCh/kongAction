bbcdd01752c006b4a885a5e056af188f12375dc5
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-23 15:49:29 +0200

    msg

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 echo "Hello sir"
 
-echo "install nvm"
-touch ~/.profile
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-export PATH=~/.npm-global/bin:$PATH
-source ~/.profile
+# echo "install nvm"
+# touch ~/.profile
+# mkdir ~/.npm-global
+# npm config set prefix '~/.npm-global'
+# export PATH=~/.npm-global/bin:$PATH
+# source ~/.profile
 
-echo "install nvm"
-# nvm install node 14
-node --version
+# echo "install nvm"
+# # nvm install node 14
+# node --version
 
 # echo "change npm's default directory"
 
@@ -22,11 +22,11 @@ npm install --unsafe-perm -g insomnia-inso
 echo "inso version"
 inso --version
 
-# echo "linting"
-# run: inso lint spec "swagger.json" --ci
+echo "linting"
+run: inso lint spec "swagger.json" --ci
 
-# echo "Generate declarative config"
-# inso generate config "swagger.json" --type declarative | tee kong.yml
+echo "Generate declarative config"
+inso generate config "swagger.json" --type declarative | tee kong.yml
 
-# echo "file:"
-# cat kong.yml
+echo "file:"
+cat kong.yml
