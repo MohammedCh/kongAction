a2962ef325b94a4e69755fcba740009429751a71
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-23 11:24:54 +0200

    sh

--- index.js
+++
@@ -1,9 +0,0 @@
-const inso = require('insomnia-inso')
-async function main() {
-    try {
-        console.log(`Hello`);
-    } catch (error) {
-        core.setFailed(error.message);
-    }}
-
-main();
