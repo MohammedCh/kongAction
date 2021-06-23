548e71d9b1b8bd9f007ff7ba58870fd96841d6ec
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-23 13:05:44 +0200

    msg

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Container image that runs your code
-FROM node:14
+FROM node:10
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,19 @@
 #!/bin/sh -l
 
 echo "Hello sir"
-# node --version
+node --version
 
-# echo "installing inso"
-# npm install -g insomnia-inso
+echo "installing inso"
+npm install -g insomnia-inso
 
-# echo "inso version"
-# inso --version
+echo "inso version"
+inso --version
 
-# echo "linting"
-# run: inso lint spec "swagger.json" --ci
+echo "linting"
+run: inso lint spec "swagger.json" --ci
 
-# echo "Generate declarative config"
-# inso generate config "swagger.json" --type declarative | tee kong.yml
+echo "Generate declarative config"
+inso generate config "swagger.json" --type declarative | tee kong.yml
 
-# echo "file:"
-# cat kong.yml
+echo "file:"
+cat kong.yml
