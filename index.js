fd564a7cfc227a74bde43606b3d9d9f96afe7a7c
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-24 12:53:34 +0200

    js

---
+++ index.js
@@ -0,0 +1,50 @@
+const { exec } = require("child_process");
+
+exec("echo 'installing inso' & npm install --unsafe-perm -g insomnia-inso", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("echo 'version:' & inso --version", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("echo 'linting:' & inso lint spec 'swagger.json'", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
+exec("echo 'Generate declarative config:' & nso generate config 'swagger.json' --type declarative | tee kong.yml", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
+
