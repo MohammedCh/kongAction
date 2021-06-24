3c1eff33a0982ec8428e05faaa1a023541567114
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-24 10:22:24 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,34 @@
 #!/bin/bash -l
 
 echo "Hello sir"
-USER_EMAIL="$1"
-USER_NAME="$2"
+# USER_EMAIL="$1"
+# USER_NAME="$2"
+ls
 
 cd ..
+ls
+
 cd ..
 
 echo "setup git"
-git init
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
-git fetch
-git checkout -m main swagger.json
+# git init
+# git config --global user.email "$USER_EMAIL"
+# git config --global user.name "$USER_NAME"
+# git fetch
+# git checkout -m main swagger.json
 ls
 
-echo "installing inso"
-npm install --unsafe-perm -g insomnia-inso
+# echo "installing inso"
+# npm install --unsafe-perm -g insomnia-inso
 
-echo "inso version"
-inso --version
+# echo "inso version"
+# inso --version
 
-echo "linting"
-inso lint spec "swagger.json"
+# echo "linting"
+# inso lint spec "swagger.json"
 
-echo "Generate declarative config"
-inso generate config "swagger.json" --type declarative | tee kong.yml
+# echo "Generate declarative config"
+# inso generate config "swagger.json" --type declarative | tee kong.yml
 
-echo "file:"
-cat kong.yml
+# echo "file:"
+# cat kong.yml
