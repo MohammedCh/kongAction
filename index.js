53d5442a74b798ca222605bc291bf24da83418e0
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-24 14:03:21 +0200

    js

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
 const { exec } = require("child_process");
 
-async function f() {
-    console.log("hiiii")
-
-    function promisifyExec(cmd) {
-        return new Promise((resolve, reject) => {
-            exec(cmd, (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
-                resolve(stdout? stdout : stderr);
-            });
-        });
-    };
-
-    function execute(cmd) {
+function promisifyExec(cmd) {
+    return new Promise((resolve, reject) => {
         exec(cmd, (error, stdout, stderr) => {
             if (error) {
                 console.log(`error: ${error.message}`);
@@ -31,14 +12,31 @@ async function f() {
                 return;
             }
             console.log(`stdout: ${stdout}`);
+            resolve(stdout ? stdout : stderr);
         });
-    }
+    });
+};
 
-    promisifyExec("echo 'installing inso' & sudo npm install --unsafe-perm -g insomnia-inso")
+function execute(cmd) {
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+try {
+    console.log("hiiii")
+    await promisifyExec("echo 'installing inso' & sudo npm install --unsafe-perm -g insomnia-inso")
     execute("echo 'version:' & inso --version");
     execute("echo 'linting:' & inso lint spec 'swagger.json'");
     execute("echo 'Generate declarative config:' & inso generate config 'swagger.json' --type declarative | tee kong.yml");
-
+} catch (error) {
+    console.log(error);
 }
-
-f();
