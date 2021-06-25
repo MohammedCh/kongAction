9fad7f77bc4caa997b93152a4ef2d2b206c5d83a
Mo Chakmakchi <56258384+MohammedCh@users.noreply.github.com>
2021-06-25 16:12:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,27 +32,12 @@ function execute(cmd) {
     });
 }
 
-function executeMsg(cmd) {
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`error: ${error.message}`);
-            return;
-        }
-        if (stderr) {
-            console.log(`stderr: ${stderr}`);
-        }
-        console.log(`stdout: ${stdout}`);
-        const msg = stdout;
-        core.setOutput("msg", msg);
-    });
-}
-
 async function f() {
     console.log("hiiii")
     await promisifyExec("echo 'installing inso' & sudo npm install --unsafe-perm -g insomnia-inso");
     execute("echo 'version:' & inso --version");
     execute(`echo 'linting:' & inso lint spec ${swaggerFile}`);
-    executeMsg(`echo 'Generate declarative config:' & inso generate config ${swaggerFile} --type declarative | tee kong.yaml`);
+    execute(`echo 'Generate declarative config:' & inso generate config ${swaggerFile} --type declarative | tee kong.yaml`);
 }
 
 f();
