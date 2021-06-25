f51f5d9f247cda3532021c6a4dcd0ff77ff2b10e
Mohammed Chakmakchi <mohammed.chakmakchi@lego.com>
2021-06-25 15:51:30 +0200

    g

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ async function f() {
     await promisifyExec("echo 'installing inso' & sudo npm install --unsafe-perm -g insomnia-inso");
     execute("echo 'version:' & inso --version");
     execute(`echo 'linting:' & inso lint spec ${swaggerFile}`);
-    execute(`echo 'Generate declarative config:' & inso generate config ${swaggerFile} --type declarative > kong.yaml`);
-    execute("cat kong.yaml")
+    execute(`echo $(inso generate config ${swaggerFile} --type declarative) > kong.yaml`)
 }
 
 f();
