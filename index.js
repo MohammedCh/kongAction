c93ed4274066944ed6e86893d89b4301c950231d
Mo Chakmakchi <56258384+MohammedCh@users.noreply.github.com>
2021-06-25 16:06:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function f() {
     await promisifyExec("echo 'installing inso' & sudo npm install --unsafe-perm -g insomnia-inso");
     execute("echo 'version:' & inso --version");
     execute(`echo 'linting:' & inso lint spec ${swaggerFile}`);
-    executeMsg(`echo 'Generate declarative config:' & inso generate config ${swaggerFile} --type declarative | tee kong.yml`);
+    execute(`echo $(inso generate config ${swaggerFile} --type declarative) > kong.yaml`)
 }
 
 f();
